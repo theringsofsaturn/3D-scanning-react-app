eeff4fcd1571af16018882f97cc15405842876fe
aurrelkondi <aurrelkondi@hotmail.com>
2024-01-15 20:04:23 +0100

    email changed

--- src/Components/CTA/cta_section.jsx
+++ src/Components/CTA/cta_section.jsx
@@ -4,36 +4,36 @@ import { images } from "../../assets/ImpoerImages";
 import "./cta_section.css";
 
 const cta_section = () => {
-  return (
-    <>
-      <div className="cta" id="get-in-touch">
-        <div className="cta_section">
-          <div className="cta_image">
-            <img src={images.AboutUs} alt="cta image" />
-          </div>
-          <div className="cta_content">
-            <h1 className="heading-text">Get In Touch</h1>
-            <p className="content-text">
-              Ready to experience the future of spatial visualization? Request a
-              demo or a personalized quote, and let ThriD transform the way you
-              see spaces.
-            </p>
-            <p className="content-text">
-              Immerse yourself in the world of ThriD by watching our videos and
-              demos. See firsthand how our technology revolutionizes spatial
-              exploration.
-            </p>
-            <a
-              href="mailto:emiliankasemi@gmail.com?subject=Request%20for%20Demo"
-              className="cta-button"
-            >
-              Request a Demo
-            </a>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="cta" id="get-in-touch">
+				<div className="cta_section">
+					<div className="cta_image">
+						<img src={images.AboutUs} alt="cta image" />
+					</div>
+					<div className="cta_content">
+						<h1 className="heading-text">Get In Touch</h1>
+						<p className="content-text">
+							Ready to experience the future of spatial visualization? Request a
+							demo or a personalized quote, and let ThriD transform the way you
+							see spaces.
+						</p>
+						<p className="content-text">
+							Immerse yourself in the world of ThriD by watching our videos and
+							demos. See firsthand how our technology revolutionizes spatial
+							exploration.
+						</p>
+						<a
+							href="mailto:sales@thrid.eu?subject=Request%20for%20Demo"
+							className="cta-button"
+						>
+							Request a Demo
+						</a>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default cta_section;
